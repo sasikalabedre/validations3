67b7b4e24b1fc25988748c157fbe04f2922a8e7b
sasikala <sasikala.bedre@gmail.com>
2025-03-12 16:21:36 +0530

    change

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace validations3.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Privadcy()
+        {
+            return View();
+        }
+
     }
 }
